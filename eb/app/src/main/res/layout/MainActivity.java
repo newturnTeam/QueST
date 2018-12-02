d85e03ff730c404466e98494946dbb0ae6dc1875
keb0120 <43941352+keb0120@users.noreply.github.com>
2018-12-02 12:42:36 +0900

    첫진입화면,로그인,회원가입 UI progressdiaglog(loading.java) 추가

---
+++ eb/app/src/main/res/layout/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.keb01.quest5;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageButton button1 = (ImageButton) findViewById(R.id.imageButton2);
+        button1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                    Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                    startActivity(intent);
+            }
+        });
+    }
+}
---
+++ eb/app/src/main/res/layout/SeatViewProf.java
@@ -0,0 +1,28 @@
+package com.example.keb01.quest5;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SeatViewProf extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seat_view_prof);
+        //액션바 가리기
+        getSupportActionBar().hide();
+        //질문방으로 화면전환
+        Button button = (Button) findViewById(R.id.questionroom);
+        button.setOnClickListener(new View.OnClickListener() {
+            //코드전달안됨 고치셈
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),QuestionRoomProf.class);
+                startActivityForResult(intent,101);
+            }
+        });
+    }
+}
